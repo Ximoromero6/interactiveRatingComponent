da9d563f69879bd0f47009d2020b6b133c02718d
Ximo Romero <ximore55@gmail.com>
2022-10-17 19:51:03 +0200

    Add initial files

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+(function() {
+    const containerRating = document.querySelector(".containerRating");
+    let ratingNumber = null;
+    const submitButton = document.getElementById("submitButton");
+    const mainContainer = document.getElementsByTagName("main")[0];
+
+    containerRating.addEventListener("click", (evt) => {
+        let closestElement = evt.target.closest(".ratingButton");
+
+        document.querySelectorAll(".ratingButton").forEach((el) => {
+            el.classList.remove("selected");
+        });
+
+        closestElement.classList.add("selected");
+        ratingNumber = closestElement.getAttribute("data-rating");
+    });
+
+
+    submitButton.addEventListener("click", () => {
+        mainContainer.classList.add("submited");
+        mainContainer.innerHTML = `
+            <div class="containerImageSubmited">
+                <img src="assets/images/illustration-thank-you.svg" alt="">
+            </div>
+            <p class="selectedRating">You selected ${ratingNumber} out of 5</p>
+            <h2 class="title">Thank you!</h2>
+            <p class="subtitle">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
+        `;
+    });
+})();
